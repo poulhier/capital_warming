00cde607b82e37cb2e3edec72d1e87d1a57b670b
poulhier <victor.poulhier@insa-cvl.fr>
2019-10-31 17:15:59 +0100

    creation d'un script start et du ficher bot.js

---
+++ bot.js
@@ -0,0 +1,10 @@
+console.log("the bot is starting");
+
+var twitter = require('twitter');
+
+var bot = new twitter({
+  consumer_key: '',
+  consumer_secret: '',
+  access_token_key: '',
+  access_token_secret: ''
+});
